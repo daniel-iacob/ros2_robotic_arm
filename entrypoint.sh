cec12e7518e2419ed7edb91701342cadf5a66b01
Daniel Iacob <daniel_iacob>
2026-01-14 21:43:25 +0200

    simplify entrypoint

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,14 +1,4 @@
 #!/bin/bash
 set -e
 
-# Used by docker to run commands after container is started
-# In this way, it will be a simpler image/ubuntu resulter from Dockerfile
-# And all the other install will be like on any other Linux
-
-# Source ROS 2 setup.bash in bashrc so that every new terminal 
-# has ROS 2 environment sourced
-if ! grep -q "source /opt/ros/jazzy/setup.bash" /root/.bashrc; then
-    echo "source /opt/ros/jazzy/setup.bash" >> /root/.bashrc
-fi
-
 exec "$@"
