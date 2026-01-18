cd851b0aea4319b8d868a93d8d17f8dfd8e02735
Daniel Iacob <daniel_iacob>
2026-01-18 19:11:25 +0200

    gui auth

--- run_container_gui.sh
+++ run_container_gui.sh
@@ -1,7 +1,18 @@
 #!/bin/bash
+set -e
 
-# Use this to start the container manually with GUI support
-# This is the manual alternative to devcontainers
-xhost +local:docker
-docker compose run ros2-robotic-arm
-xhost -local:docker
+XAUTH=/tmp/.docker.xauth
+
+
+# Give permission for container to open the applications with have windows
+# If it's a directory (the Docker mistake), delete it. Some random error
+if [ -d "$XAUTH" ]; then
+    echo "Fixing Docker directory mistake..."
+    sudo rm -rf "$XAUTH"
+fi
+if [ ! -f "$XAUTH" ]; then # Create the file if it doesn't exist
+    touch "$XAUTH"
+fi
+xauth nlist $DISPLAY | sed -e 's/^..../ffff/' | xauth -f $XAUTH nmerge - # Fill with X11 cookie
+
+docker compose run --remove-orphans ros2-robotic-arm
