3b87727e1c70ef21f49b5b99e6fea3b1ea7294b6
Daniel Iacob <daniel_iacob>
2026-01-18 19:12:30 +0200

    c

--- run_container_gui.sh
+++ run_container_gui.sh
@@ -1,11 +1,9 @@
 #!/bin/bash
 set -e
 
-XAUTH=/tmp/.docker.xauth
-
-
 # Give permission for container to open the applications with have windows
 # If it's a directory (the Docker mistake), delete it. Some random error
+XAUTH=/tmp/.docker.xauth
 if [ -d "$XAUTH" ]; then
     echo "Fixing Docker directory mistake..."
     sudo rm -rf "$XAUTH"
@@ -14,5 +12,6 @@ if [ ! -f "$XAUTH" ]; then # Create the file if it doesn't exist
     touch "$XAUTH"
 fi
 xauth nlist $DISPLAY | sed -e 's/^..../ffff/' | xauth -f $XAUTH nmerge - # Fill with X11 cookie
+
 
 docker compose run --remove-orphans ros2-robotic-arm
