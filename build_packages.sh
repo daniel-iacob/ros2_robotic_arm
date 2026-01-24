649b977705506b705cd7d1f56dfd7abadcb015f7
Daniel Iacob <daniel_iacob>
2026-01-24 21:12:45 +0200

    build all packages

--- build_packages.sh
+++ build_packages.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
-set -e
 
+# Build custom defined packages
+# It should be run inside conainer
+
+set -e
 source /opt/ros/jazzy/setup.bash
-colcon build --symlink-install --packages-select robotic_arm_description
+colcon build --symlink-install
 source install/setup.bash
