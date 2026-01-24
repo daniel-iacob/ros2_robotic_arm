9f7451c11ccc68e579b47e4b8673a1d30894eaf5
Daniel Iacob <daniel_iacob>
2026-01-24 21:12:22 +0200

    combine build and run container

--- build_and_run_container.sh
+++ build_and_run_container.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 set -e
 
+docker build -t ros2-robotic-arm .
+
 # Give permission for container to open the applications with have windows
 # If it's a directory (the Docker mistake), delete it. Some random error
 XAUTH=/tmp/.docker.xauth
@@ -12,6 +14,5 @@ if [ ! -f "$XAUTH" ]; then # Create the file if it doesn't exist
     touch "$XAUTH"
 fi
 xauth nlist $DISPLAY | sed -e 's/^..../ffff/' | xauth -f $XAUTH nmerge - # Fill with X11 cookie
-
 
 docker compose run --remove-orphans ros2-robotic-arm
