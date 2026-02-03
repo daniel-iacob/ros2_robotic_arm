ee0237b9904b222f5874c55a83abfaa1bd85c5fe
Daniel Iacob <daniel_iacob>
2026-02-03 23:30:08 +0200

    add a cube in scene

--- src/robotic_arm_bringup/launch/arm_system.launch.py
+++ src/robotic_arm_bringup/launch/arm_system.launch.py
@@ -4,6 +4,7 @@ from ament_index_python.packages import get_package_share_directory
 from launch import LaunchDescription
 from launch.actions import IncludeLaunchDescription
 from launch.launch_description_sources import PythonLaunchDescriptionSource
+from launch_ros.actions import Node
 
 
 def generate_launch_description():
@@ -11,16 +12,16 @@ def generate_launch_description():
     pkg_moveit_config = get_package_share_directory("robotic_arm_moveit_config")
 
     # 2. Include the MoveIt "demo" launch
-    # This automatically handles:
-    # - robot_state_publisher (The skeleton)
-    # - move_group (The brain)
-    # - rviz (The eyes)
-    # - ros2_control/fake_hardware (The muscles)
     moveit_demo = IncludeLaunchDescription(
         PythonLaunchDescriptionSource(os.path.join(pkg_moveit_config, "launch", "demo.launch.py"))
     )
 
-    # 3. (Optional) Add your custom script launch here later
-    # For now, we just launch the system infrastructure.
+    # 3. Define the Scene Manager Node
+    scene_manager_node = Node(
+        package="robotic_arm_bringup",
+        executable="scene_manager",
+        name="scene_manager",
+        output="screen",
+    )
 
-    return LaunchDescription([moveit_demo])
+    return LaunchDescription([moveit_demo, scene_manager_node])
